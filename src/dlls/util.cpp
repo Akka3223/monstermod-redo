36081f0b03e1b60ec923fbfec4636cf921103b8e
Akka3223 <58777333+Akka3223@users.noreply.github.com>
2023-07-27 07:49:24 +0300

    remove monstermod damage (we doin that via diablo)

--- src/dlls/util.cpp
+++ src/dlls/util.cpp
@@ -1915,7 +1915,7 @@ int UTIL_TakeDamage( edict_t *pEdict, entvars_t *pevInflictor, entvars_t *pevAtt
 	}
 
 	// do the damage
-	pEdict->v.health -= flTake;
+	//pEdict->v.health -= flTake;
 	char extCmd[64];
 	sprintf( extCmd, "_ptakedamage %i %i %f\n", ENTINDEX( ENT( pevAttacker ) ), ENTINDEX( pEdict ), flDamage );
 	SERVER_COMMAND( extCmd );
