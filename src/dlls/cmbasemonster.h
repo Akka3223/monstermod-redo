33b84121bdbf2d7caaec0037e7d5ad6a031a5437
Akka3223 <58777333+Akka3223@users.noreply.github.com>
2025-05-23 02:51:04 +0300

    Update cmbasemonster.h
    + Tier 2 Monster HP: [300 -> 250]
    + Tier 3 Monster HP: [1000 -> 850]
    + Tier 4 Monster HP: [1800 -> 1700]

--- src/dlls/cmbasemonster.h
+++ src/dlls/cmbasemonster.h
@@ -25,9 +25,9 @@
 #endif
 
 const float Tier1_HP = 100.0;
-const float Tier2_HP = 300.0;
-const float Tier3_HP = 1000.0;
-const float Tier4_HP = 1800.0;
+const float Tier2_HP = 250.0;
+const float Tier3_HP = 850.0;
+const float Tier4_HP = 1700.0;
 const float Tier5_HP = 3200.0;
 const float Tier6_HP = 12000.0;
 const float Tier7_HP = 850.0;
